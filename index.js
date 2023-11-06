9a2a4836d4c058c35775bac2a20f4d2bf3c72e53
Patrick Annan <you@example.compatrickannan@hotmail.com>
2023-11-06 17:12:23 -0500

    first push

---
+++ index.js
@@ -0,0 +1,118 @@
+var number = 5; // in-line comment
+
+/* this is a multi line comment */
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object */
+
+var myName = "Patrick"
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+var a;
+var b = 2;
+console.log(a);
+a = 7;
+b = a;
+console.log(a);
+var a = 9;
+
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+var sum = 10 + 10;
+console.log(sum)
+
+var difference = 45 - 33;
+
+var product = 8 * 10;
+
+var quotient = 66 / 33;
+
+var myVar = 87;
+myVar ++;
+
+myVar--;
+
+var ourDecimal = 5.7;
+var myDecimal = 0.009;
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var remainder;
+remainder = 11 % 3;
+
+a += 12;
+b += 9;
+c += 7;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+a /= 12;
+a /= 4;
+c /= 11;
+
+var myFirstName = "Patrick";
+var myLastName = "Annan";
+
+var MyStr = "I am a \"double quoted\" string inside \"double quotes\";
+
+var myStr = '<a href="http:www.example.com">'
+
+var ourStr = "I come first." + "This is the end.";
+var myStr = "This is the start. " + "This is the end.";
+console.log(myStr);
+
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+var myStr = "This is the first sentence. "
+myStr += "This is the second sentence."
+console.log(myStr);
+
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+var myName = "Patrick";
+var myStr = "My name is ";
+
+var anAdjective = "Awesome!";
+var ourStr = "freeCodeCamp is";
+ourStr += anAdjective;
+var someAdjective = "worthwhile";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+lastNameLength = lastName.length;
+
+
+var firstLetterOfLastName = "";
+var lastName ="Annan";
+
+firstLetterOfLastName = lastName[0];
+
+var myStr = "Jello World";
+myStr = "Hello World"; 
+
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+var lastName = "Lovelace";
+var thirdLetterOfLastName = lastName[2];
+
+var firstName ="Ada";
+var lastLetterOfFirstName = firstName[firstName.length -1];
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1];
